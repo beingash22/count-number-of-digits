d3b39ebac631bef5216b54db3d760f5e110755b0
beingash22 <ashrockzap@gmail.com>
2018-01-28 19:03:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,17 +5,13 @@ void main ()
 
   int num, count=0;
   scanf ("%d", &num);
-
-  if (num>0)
+  
+  while (num>0)
   {
-    while (num>0)
-    {
-      count++;
-      num=num/10;
-    }
-    
-    printf ("%d", count);
+    count++;
+    num=num/10;
   }
-  else
-    printf ("invalid input");
+    
+  printf ("%d", count);
 }
+
